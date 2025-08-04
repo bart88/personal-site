5926499d8f9d2d240dcda4a2f047df59332d0c09
Chris Barton <cjbarton01@gmail.com>
2025-08-04 11:10:19 +0930

    Bye LangtonsAnt vibecoded nonsense, get some cpu back

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,8 @@
 import HomePageIsland from "../islands/HomePageIsland.tsx";
-import LangtonsAnt from "../islands/LangtonsAnt.tsx";
 
 export default function Home() {
   return (
     <div class="min-h-screen w-full bg-white relative">
-      {/* Langton's Ant Background Animation */}
-      <LangtonsAnt 
-        width={1920}
-        height={1080}
-        cellSize={12}
-        animationSpeed={30}
-        opacity={0.15}
-      />
       <HomePageIsland />
     </div>
   );
